a654d315960eb70d9fddb255434bfe661a332305
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-09-29 23:17:28 +0530

    Added Document Title Editing & Storing Feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -3,7 +3,7 @@
 import {nanoid} from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
-import { parseStringify } from "../utils";
+import { getAccessType, parseStringify } from "../utils";
 
 
 export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
@@ -47,5 +47,56 @@ export const getDocument = async ({ roomId, userId }: { roomId: string; userId: 
       return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while getting a room: ${error}`);
+  }
+}
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title
+      }
+    })
+
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error happened while updating a room: ${error}`);
+  }
+}
+
+export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy }: ShareDocumentParams) => {
+  try {
+    const usersAccesses: RoomAccesses = {
+      [email]: getAccessType(userType) as AccessType,
+    }
+
+    const room = await liveblocks.updateRoom(roomId, { 
+      usersAccesses
+    })
+
+    if(room) {
+      const notificationId = nanoid();
+
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: '$documentAccess',
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email
+        },
+        roomId
+      })
+    }
+
+    revalidatePath(`/documents/${roomId}`);
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while updating a room access: ${error}`);
   }
 }
